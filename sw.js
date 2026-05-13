92d528c325edd7aa0a988d69eb55f94f7f1e5fdc
nshyun2001-source <nshyun2001@gmail.com>
2026-05-13 17:00:59 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'fishing-note-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon-512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
